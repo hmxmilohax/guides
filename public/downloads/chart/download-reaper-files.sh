0e1dd166ce3e842bd3fc22a5b6c427e571195763
Carl Mylo <56451149+carlmylo@users.noreply.github.com>
2026-03-16 23:28:26 -0500

    no message

--- public/downloads/chart/download-reaper-files.sh
+++ public/downloads/chart/download-reaper-files.sh
@@ -1,4 +1,4 @@
-curl https://www.dropbox.com/scl/fi/9zxvaa36zc5lnrt83tfb6/REAPER.Files.Linux.zip?rlkey=oj50b0pqvgatj2q7k7sao5pp4&st=64a2k1do&dl=0 -L
+curl https://www.dropbox.com/scl/fi/9zxvaa36zc5lnrt83tfb6/REAPER.Files.Linux.zip?rlkey=oj50b0pqvgatj2q7k7sao5pp4&st=64a2k1do&dl=1 -L
 unzip -o REAPER.Files.Linux.zip
 cd thefiles
 bash install-reaper-files.sh
